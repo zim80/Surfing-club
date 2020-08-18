b74810bb61d3c0226db1b554ab3cd59c901c9b87
Maxim Zaytsev <zim@bobru.net>
2020-08-18 15:42:25 +0300

    Поправил несоответствие версий node и gulp

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,22 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-      items: 1,
-      loop: true,
-      dots: false,
-      smartSpeed: 2000
-    });
+$(document).ready(function () {
+
+  const headerSlider = $('.owl-carousel');
+  headerSlider.owlCarousel({
+    items: 1,
+    loop: true,
+    dots: false,
+    smartSpeed: 2000
   });
+
+
+
+  $('#headerSliderRight').click(function () {
+    headerSlider.trigger('next.owl.carousel');
+  })
+
+  $('#headerSliderLeft').click(function () {
+
+    headerSlider.trigger('prev.owl.carousel', [300]);
+  })
+
+});
